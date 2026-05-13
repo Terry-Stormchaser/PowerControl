7e0da40af663b4bce17b406fb6634a8fb9f76888
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-05-12 22:33:34 -0400

    Update Reinstall_PowerControl.sh

--- bin/Reinstall_PowerControl.sh
+++ bin/Reinstall_PowerControl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_PowerControl/main/bin/PowerControl_Installer.sh -o ~/ChromeOS_PowerControl_Installer.sh
 sudo mkdir -p /usr/local/bin
-sudo -E bash ~/ChromeOS_PowerControl_Installer.sh
+sudo -E bash ~/Install_PowerControl.sh
