e29bef12fdb9763b93889d43c1b2272bd61aec8c
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-05-13 00:49:22 -0400

    Update Download_PowerControl.sh

--- bin/Download_PowerControl.sh
+++ bin/Download_PowerControl.sh
@@ -7,8 +7,26 @@ MAGENTA=$(tput setaf 5)
 CYAN=$(tput setaf 6)
 BOLD=$(tput bold)
 RESET=$(tput sgr0)
-rm -f ~/Install_PowerControl.sh 2>/dev/null
-curl -L https://raw.githubusercontent.com/shadowed1/PowerControl/main/bin/Install_PowerControl.sh -o ~/Install_PowerControl.sh
-chmod +x ~/Install_PowerControl.sh
-sudo mkdir -p /usr/local/bin
-sudo -E bash ~/Install_PowerControl.sh
+echo
+while true; do
+    read -rp "${YELLOW}${BOLD}Install PowerControl?${RESET}${BOLD} (Y/n): ${RESET}" confirm
+    case "$confirm" in
+        [Yy]* | "")
+            rm -f ~/Install_PowerControl.sh 2>/dev/null
+            curl -L https://raw.githubusercontent.com/shadowed1/PowerControl/main/bin/Install_PowerControl.sh -o ~/Install_PowerControl.sh
+            chmod +x ~/Install_PowerControl.sh
+            sudo mkdir -p /usr/local/bin
+            sudo -E bash ~/Install_PowerControl.sh
+            break
+            ;;
+        [Nn]*)
+            echo -e "${BLUE}Cancelled.${RESET}\n"
+            ;;
+        *)
+            echo -e "${RED}Please answer Y/n.${RESET}"
+            ;;
+    esac
+done
+
+
+
